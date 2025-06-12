1461d027c4a9c896624c0f97867de50c7a2d7dc1
jkd98 <josesitoelalto@gmail.com>
2025-06-12 09:05:28 -0600

    Maquetado home page completo

---
+++ front-events/src/app/events/components/hero-organizador/hero-organizador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-organizador',
+  imports: [],
+  templateUrl: './hero-organizador.component.html',
+  styleUrl: './hero-organizador.component.css'
+})
+export class HeroOrganizadorComponent {
+
+}
--- front-events/src/app/events/pages/events-home-page/events-home-page.component.ts
+++ front-events/src/app/events/pages/events-home-page/events-home-page.component.ts
@@ -3,9 +3,18 @@ import { NavBarComponent } from '../../../shared/components/nav-bar/nav-bar.comp
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { AreasInteresComponent } from '../../components/areas-interes/areas-interes.component';
 import { PopularEventsComponent } from '../../components/popular-events/popular-events.component';
+import { HeroOrganizadorComponent } from '../../components/hero-organizador/hero-organizador.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
-  imports: [NavBarComponent,HeaderComponent,AreasInteresComponent,PopularEventsComponent],
+  imports: [
+    NavBarComponent,
+    HeaderComponent,
+    AreasInteresComponent,
+    PopularEventsComponent,
+    HeroOrganizadorComponent,
+    FooterComponent
+  ],
   templateUrl: './events-home-page.component.html',
   styleUrl: './events-home-page.component.css'
 })
